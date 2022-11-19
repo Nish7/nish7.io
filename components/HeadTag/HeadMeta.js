23d07f9f0feb92e7d9cfb2645c69f59e40c0cead
Nish7 <nishilkapadia17@gmail.com>
2022-11-19 02:34:22 -0500

    fix meta tag image

--- components/HeadTag/HeadMeta.js
+++ components/HeadTag/HeadMeta.js
@@ -10,14 +10,26 @@ function HeadMeta({ title }) {
 			<meta property="og:site_name" content="nish7.io" />
 			<meta property="og:title" content={title} />
 			<meta property="og:type" content="article" />
-			<meta property="og:image" content={`/card_social.png`} />
-			<meta property="og:image:url" content={`/card_social.png`} />
-			<meta property="og:image:secure_url" content={`/card_social.png`} />
+			<meta
+				property="og:image"
+				content={`https://www.nish7.io/card_social.png`}
+			/>
+			<meta
+				property="og:image:url"
+				content={`https://www.nish7.io/card_social.png`}
+			/>
+			<meta
+				property="og:image:secure_url"
+				content={`https://www.nish7.io/card_social.png`}
+			/>
 
 			{/* Twitter Cards */}
 			<meta name="twitter:title" content={title} />
 			<meta name="twitter:url" content="nish7.io" />
-			<meta name="twitter:image" content={'/card_social.png'} />
+			<meta
+				name="twitter:image"
+				content={'https://www.nish7.io/card_social.png'}
+			/>
 			<meta name="twitter:card" content="summary_large_image" />
 		</Head>
 	);
