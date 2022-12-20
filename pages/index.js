0de81549c6b9e0480899a1d1d91614526796bdf6
Nish7 <nishilkapadia17@gmail.com>
2022-12-20 13:25:54 -0500

    index page ui change

--- pages/index.js
+++ pages/index.js
@@ -11,24 +11,28 @@ export default function Home({ workData, educationData }) {
 			<HeadMeta title="Nishil Kapadia | Home" />
 
 			<Box w={['90%', '90%', '70%']} py={8} mx="auto" my={[16, 16, 0]}>
-				<Flex mb={14} align="center">
-					<Box>
-						<Text
-							fontWeight="bold"
-							fontSize="4xl"
-							mx="auto"
-							w="auto"
-							letterSpacing={-0.25}
-						>
-							Nishil Kapadia
-						</Text>
-						<Text fontWeight="semibold">Software Developer</Text>
-						<Text mt={7} w="80%" letterSpacing={0.5}>
-							Software Developer with a full-stack experience, primarily working
-							with Java(Type)Script. I am also an undergraduate computer science
-							student at Toronto Metropolitan University, Toronto.
-						</Text>
-					</Box>
+				<Flex mb={14} align="center" flexDir={'column'}>
+					<Text
+						fontWeight="bold"
+						fontSize="4xl"
+						mx="auto"
+						w="auto"
+						letterSpacing={-0.25}
+					>
+						Nishil Kapadia
+					</Text>
+					<Text fontWeight="semibold">Software Developer</Text>
+					<Text
+						mt={7}
+						w="80%"
+						letterSpacing={0.5}
+						textAlign={['center', 'center', 'left']}
+					>
+						Software Developer with full-stack experience, primarily working
+						with Java(Type)Script. I am also an undergraduate computer science
+						student at Toronto Metropolitan University. Right now I&apos;m
+						building modern web applications and software at The Eyeopener.
+					</Text>
 				</Flex>
 
 				{/* <FeaturePost /> */}
