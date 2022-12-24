c74a719eec6a4c1de2778985f3da09f86c155f6c
Nish7 <nishilkapadia17@gmail.com>
2022-12-24 14:45:02 -0500

    bug sidebar title link

--- src/components/navbar/SectionTitle.js
+++ src/components/navbar/SectionTitle.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 export default function SectionTitle({ p_href = '/', children }) {
 	return (
-		<Link href={p_href} passHref>
-			<Text fontSize="sm" fontWeight="semibold" color="gray.400" pl={3} mb={2}>
+		<Text fontSize="sm" fontWeight="semibold" color="gray.400" pl={3} mb={2}>
+			<Link href={p_href} passHref>
 				{children}
-			</Text>
-		</Link>
+			</Link>
+		</Text>
 	);
 }
